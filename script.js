637376c7dd7760a55a795a26101edaf039c6410a
Moussi Mohamed Amine <100562439+Moussi0amine@users.noreply.github.com>
2025-10-04 10:32:15 +0100

    add controls logic

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ let playerPosition = 175;
 let enemies = [];
 let speed = 5;
 let score = 0;
+
+// Key controls
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !gameRunning) startGame();
+    if (keys.hasOwnProperty(e.key)) keys[e.key] = true;
+});
+
+document.addEventListener("keydown", (e) =>{
+    if (keys.hasOwnProperty(e.key)) keys[e.key] = false;
+});
+
