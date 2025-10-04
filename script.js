f1e0301f5972ef3c1fd0f87ca438d51285135604
Moussi Mohamed Amine <100562439+Moussi0amine@users.noreply.github.com>
2025-10-04 10:42:25 +0100

    first functions

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ document.addEventListener("keydown", (e) =>{
     if (keys.hasOwnProperty(e.key)) keys[e.key] = false;
 });
 
+function startGame() {
+    startScreen.style.display = "none";
+    gameRunning = true;
+    score = 0;
+    enemies = [];
+    playerPosition = 175;
+    player.style.left = playerPosition + "px";
+    gameArea.appendChild(player);
+    spawnEnemis();
+    window.requestAnimationFrame(gameLoop);
+}
+
+function spawnEnemis() {
+    for (let i = 0; i < 3; i++){
+        let enemy = documen.createElement("div");
+        enemy.classList.add("enemy");
+        resetEnemy(enemy);
+        gameArea.appendChild(enemy);
+        enemies.push(enemy);
+    }
+}
+
+function resetEnemy(enemy) {
+    enemy.style.top = Math.random() * -300 + "px";
+    enemy.style.left = Math.floor(Math.random() * 120 + 40 + "px");
+}
