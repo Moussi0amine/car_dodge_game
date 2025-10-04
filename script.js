d9e59fa4e8c5d6689615a94a626822c66ef5574f
Moussi Mohamed Amine <100562439+Moussi0amine@users.noreply.github.com>
2025-10-04 11:02:21 +0100

    finalise functions

--- script.js
+++ script.js
@@ -45,3 +45,54 @@ function resetEnemy(enemy) {
     enemy.style.top = Math.random() * -300 + "px";
     enemy.style.left = Math.floor(Math.random() * 120 + 40 + "px");
 }
+
+function gameLoop() {
+    if (!gameRunning) return;
+
+    movePlayer();
+    moveEnemies();
+    score ++;
+    window.requestAnimationFrame(gameLoop); 
+}
+
+function movePlayer() {
+    if (keys.ArrowLeft && playerPosition > 40) playerPosition -= 5;
+    if (keys.ArrowRight && playerPosition > 310) playerPosition += 5;
+    player.style.left = playerPosition + "px";
+}
+
+function moveEnemies() {
+    enemies.forEach((enemy) => {
+        let top = parseInt(enemy.style.top);
+        top += speed;
+
+        if (top > 600) {
+            resetEnemy(enemy);
+        } else {
+            enemy.style.top = top + "px";
+        }
+
+        // Collision detection
+        if (isColliding(player, enemy)) {
+            endGame();
+        }
+    });
+}
+
+function isColliding(a, b) {
+    const aRect = a.getBoundingClientRect();
+    const bRect = b.getBoundingClientRect();
+
+    return !(
+        aRect.bottom < bRect.top ||
+        aRect.top > bRect.bottom ||
+        aRect.right < bRect.left ||
+        aRect.left < bRect.right 
+    );
+}
+
+function endGame() {
+    gameRunning = false;
+    startScreen.style.display = "block";
+    startScreen.innerHTML = `<h1>💥 Game Over!</h1><p>Score: ${score}</p><p>Press ENTER to Restart</p>`;
+}
